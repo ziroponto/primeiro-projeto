4f40ca1f3ee02e6f2211ab15584941d8e328bd8e
ziroponto <135622441+ziroponto@users.noreply.github.com>
2023-06-05 10:23:28 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,44 @@
+var box;
+
+function setup() {
+  createCanvas(400,400);
+  box = createSprite(200,200,30,30);
+}
+
+function draw() 
+{
+  background(30);
+
+  
+
+  if (keyIsDown(RIGHT_ARROW))
+  {
+    box.position.x = box.position.x +5;
+    background  ("blue");
+  }
+
+  if (keyIsDown(LEFT_ARROW))
+  {
+    box.position.x = box.position.x -5;
+    background ("red");
+  }
+
+  if (keyIsDown(UP_ARROW))
+  {
+    box.position.y = box.position.y -5;
+    background ("green");
+  }
+
+  if (keyIsDown(DOWN_ARROW))
+  {
+    box.position.y = box.position.y +5;
+    background ("yellow");
+  }
+
+  drawSprites();
+
+}
+
+
+
+
